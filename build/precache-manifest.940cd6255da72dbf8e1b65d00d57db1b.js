15210be82cd89361cfa43282f3f28b2f1c1d9aef
Wendy Yu <y_jies@hotmail.com>
2020-11-08 13:46:17 +0800

    up

---
+++ build/precache-manifest.940cd6255da72dbf8e1b65d00d57db1b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f204ad3a39043b72980c3975216736b",
+    "url": "/Weather_App_Redux1/index.html"
+  },
+  {
+    "revision": "d4af012fa424a3234734",
+    "url": "/Weather_App_Redux1/static/css/main.a97482d6.chunk.css"
+  },
+  {
+    "revision": "96c9a6389def8776c278",
+    "url": "/Weather_App_Redux1/static/js/2.915345b3.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/Weather_App_Redux1/static/js/2.915345b3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d4af012fa424a3234734",
+    "url": "/Weather_App_Redux1/static/js/main.44b1f653.chunk.js"
+  },
+  {
+    "revision": "d18141e7ac23578935f2",
+    "url": "/Weather_App_Redux1/static/js/runtime-main.bd456dd8.js"
+  }
+]);
--- src/util/axios.js
+++
@@ -1,6 +0,0 @@
-import axios from 'axios';
-
-export const getWeather = city => {
-    return axios.get(`https://jr-weather-api.herokuapp.com/api/weather?cc=Australia&city=${city}`)
-        .then(res => res.data.data);
-}
