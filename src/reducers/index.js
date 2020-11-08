8cd639a75c96146d3ff7a02b8589bbb34b1d907a
Wendy Yu <y_jies@hotmail.com>
2020-11-08 12:11:41 +0800

    update gitignore

--- src/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import {combineReducers} from 'redux';
-import {navigationReducer} from './navigationReducer';
-import {weatherReducer } from './weatherReducer';
-
-export const reducers = combineReducers ({
-    navigation:navigationReducer,
-    weather:weatherReducer,
-});
-
